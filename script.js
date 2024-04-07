8232d7e9dfe0146efaf0f7f8ca7b56d3d9cd61aa
RAMYA5577 <121097897+RAMYA5577@users.noreply.github.com>
2024-04-07 12:16:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function stringChop(str, size) {
   // your code here
+	if(str.length<size){
+		return str;
+	}
+	let count=0;
+	let arr=[];
+	let index=0;
+	// let element="";
+	for(let i=1;i<str.length;i++){
+		if(size%i==0){
+			count=i;
+			let element=str.splice(0,count);
+			arr[index]=element;
+			index++;
+		}
+		if(str.length<size){
+			arr[index+]=str;
+		}
+		
+	}
+	return arr;
 }
 
 // Do not change the code below
